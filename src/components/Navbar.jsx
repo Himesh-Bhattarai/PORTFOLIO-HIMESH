0bd3287a659a663a609c39b5001640cf83384ad9
Himeshchanchal Bhattarai <144266989+Himesh-Bhattarai@users.noreply.github.com>
2025-03-31 22:40:21 +0545

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,7 +64,7 @@ export default function Navbar() {
             <img
               src="/loog-hcb.png"
               alt="HCB Logo"
-              className="h-12 w-auto inline-block"
+              className="h-8 w-auto inline-block"
             />
           </span>
         </Link>
